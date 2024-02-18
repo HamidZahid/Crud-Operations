4b39f3d21c54b1df690ac7a8ee30fc78e372ebe8
HamidZahid <hamidchaudhary3867@gmail.com>
2024-02-18 21:24:03 +0500

    add

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.get("/", (req, res) => {
   res.send("Hello From Node API ");
 });
 
-// app.get("/api/products", async (req, res) => {
-//     try {
-//         const products = await Product.find({});
-//         res.status(200).json(products);
-//     } catch (error) {
-//         res.status(500).json({message: error.message})
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
         
-//     }
-//   });
+    }
+  });
 
 
   app.get("/api/products/:id", async (req, res) => {
