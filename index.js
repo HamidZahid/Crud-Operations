61a7487c3f59e1ac29cb606496b8300eb7981a70
HamidZahid <hamidchaudhary3867@gmail.com>
2024-02-18 21:25:09 +0500

    add

--- index.js
+++ index.js
@@ -40,15 +40,15 @@ app.get("/", (req, res) => {
   // });
 
 
-app.post("/api/products", async (req, res) => {
- try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product)
- } catch (error) {
-    res.status(500).json({message: error.message})
+// app.post("/api/products", async (req, res) => {
+//  try {
+//     const product = await Product.create(req.body);
+//     res.status(200).json(product)
+//  } catch (error) {
+//     res.status(500).json({message: error.message})
     
- }
-});
+//  }
+// });
 
 // update a product
 
